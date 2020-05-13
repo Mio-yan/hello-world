dd37a5ce9ad2afad8bb82e5ee7035fa965da3e05
梦凉廊上 <65061961+Mio-yan@users.noreply.github.com>
2020-05-14 07:30:28 +0800

    Sphere_U3D
    unity中球自动增大和缩小，幅度以及频率可通过range和rate属性更改(需要在编辑面板控制)，以后可以加UI来控制

---
+++ SphereControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereControl : MonoBehaviour {
+
+    public GameObject sphere;
+    public Vector3 scale;
+    public float radius = 1;
+    public float radiusmin = 1;
+    public float radiusmax = 3;
+    public float time;
+    private bool go = false;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (go && radius >= radiusmax)
+            go = false;
+        else if (!go && radius <= radiusmin)
+            go = true;
+        if (!go)
+        {
+            radius -= (radiusmax-radiusmin) / time;
+        }
+        else if (go)
+        {
+            radius += (radiusmax-radiusmin) / time;
+        }
+        scale = new Vector3(radius, radius, radius);
+        sphere.transform.localScale = scale;
+	}
+}
